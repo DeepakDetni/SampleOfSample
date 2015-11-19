848e271338b1b1cad5f027aefc018c94488ea753
kumaraswamyDR <kumarswamy1432m@gmail.com>
2015-11-19 13:13:03 +0530

    Sign up with google plus is implemented

--- app/src/main/java/com/app/sampleofsample/sampleofsample/MainActivity.java
+++ app/src/main/java/com/app/sampleofsample/sampleofsample/MainActivity.java
@@ -2,14 +2,27 @@ package com.app.sampleofsample.sampleofsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 //Created by Deepak D S
 public class MainActivity extends AppCompatActivity {
-
+RelativeLayout SignUpWithGoogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignUpWithGoogle = (RelativeLayout) findViewById(R.id.sign_in_with_google);
+        SignUpWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+            }
+        });
+        
         //First commit from Kumar Swamy
+    }
+
+    private void signInWithGoogle() {
     }
 }
